e55b047e6005f8af4fb837c63c08b00f59fcdfd1
YannLeNorment <yann.lenorment@gmail.com>
2020-06-04 09:45:40 +0200

    Ajout d'un plan de test pour l'ecriture dans les fichiers

---
+++ tests/write_tests.m
@@ -0,0 +1,45 @@
+function write_tests
+    %% SETUP
+
+    testCase = matlab.unittest.TestCase.forInteractiveUse;
+    files = ["",...
+            "", ...
+            ];
+        
+    %% Expectations
+    
+    
+    
+    
+    %% Functions
+    
+    % Check if the number of line is corresponding to what is expected
+    % after we write data
+    function lineNumber
+        % Before
+        
+        % After
+        
+    end
+
+    % Check if the number of columns is corresponding to what is expected
+    % after we write data
+    function columnNumber
+        % Before
+        
+        % After
+        
+    end
+
+    % Check if the data written to the file is what we added
+    function addedData(file, expectedData, line)
+        
+    end
+    
+    
+    %% TESTS
+    
+    
+    
+end
+    
