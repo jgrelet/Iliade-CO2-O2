3b1e3c0c6a0d0d20517cd414e98d2046588c7bff
YannLeNorment <yann.lenorment@gmail.com>
2020-06-08 16:39:14 +0200

    L'ecriture se fait mais les données ecrite ne sont pas correctesé

--- interpTSG_CO2_O2.m
+++ interpTSG_CO2_O2.m
@@ -69,7 +69,7 @@ function interpTSG_CO2_O2(co2interpCo2_O2File, o2File, varargin)
                 ];
                 %'OXYGEN_RAW;OXYGEN_ADJ;SATURATION;O2_TEMPERATURE;'...
             
-    StrucOrder = {'DAYD';'DATE';'GPS_TIME';'TYPE';'ERROR';'LATX';'LONX';...
+    StrucOrder = {'DATE';'GPS_TIME';'TYPE';'ERROR';'LATX';'LONX';...
                 'LATX_INT';'LONX_INT';...
                 'EQU_T';'STD';'CO2_RAW';'CO2_PHYS';'H2O_RAW';'H2O_PHYS';...
                 'LICOR_T';'LICOR_P';'ATM_P';'EQU_P';'H2O_FLOW';'LICOR_FLOW';...
@@ -101,10 +101,10 @@ function interpTSG_CO2_O2(co2interpCo2_O2File, o2File, varargin)
         msg_error = ['Open file error : ' FileOut];
         warndlg( msg_error, 'ASCII error dialog');
     else
-        disp(co2);
-        disp(StrucOrder);
-        co2b = orderfields(co2, StrucOrder);
-
+        gps_time = char(cellstr(co2.GPS_TIME));
+        type = char(cellstr(co2.TYPE));
+        
+        
         c = struct2cell(co2);
         % Number of columns
         n = size(c,1);
@@ -112,15 +112,14 @@ function interpTSG_CO2_O2(co2interpCo2_O2File, o2File, varargin)
         m = size(c{1},1);
 
         x = cell2mat(c(5:n));
-        
-        y = reshape(x,m,n-4);
 
         [Y, M, D, H, MN, S] = datevec(c{1});
         date = [D,M,Y,H,MN,S];
 
         fprintf( fidOut,'%s\n', HeaderOut);
         for i=1:m
-            %fprintf( fidOut, formatOut, date(i,:), co2.GPS_TIME(i,:), co2.TYPE(i,:),y(i,:));
+            fprintf( fidOut, formatOut, date(i,:), gps_time(i,:),...
+                type(i,:),x(i,:));
         end
         
         fclose(fidOut);
@@ -132,7 +131,8 @@ function [co2] = interpCo2_O2(co2, o2)
 
     PARA = {'OXYGEN_RAW', 'SATURATION', 'TEMPERATURE'};
     % Conversion of the date in serial date
-    dayd = serialDate(co2);
+    format = 'dd/mm/yyyy HH:MM:SS';
+    co2.DAYD = datenum(co2.DATE_TIME, format);
     for par = PARA
         par = char(par);
         
@@ -148,27 +148,27 @@ function [co2] = interpCo2_O2(co2, o2)
                 return;
         end
         
-        co2.(par)    = defaultPARA * ones(size(dayd));
+        co2.(par)    = defaultPARA * ones(size(co2.DAYD));
         
         % Definition of time borders for the interpCo2_O2olation of co2 and o2 data
         % We are looking for o2.DAYD(1) < co2.DAYD < o2.DAYD(end)
         indmin = 1;
-        if dayd(1) < o2.DAYD(1)
-            A = find( dayd > o2.DAYD(1) );
+        if co2.DAYD(1) < o2.DAYD(1)
+            A = find( co2.DAYD > o2.DAYD(1) );
             if ~isempty(A)
                 indmin = A(1);
             end
         end
 
-        indmax = size(dayd,1);
-        if dayd(end) > o2.DAYD(end)
-            A = find( dayd < o2.DAYD(end) );
+        indmax = size(co2.DAYD,1);
+        if co2.DAYD(end) > o2.DAYD(end)
+            A = find( co2.DAYD < o2.DAYD(end) );
             if ~isempty(A)
                 indmax = A(end);
             end
         end
         
-        co2.(par)(indmin:indmax) = interp1(o2.DAYD, o2.(par), dayd(indmin:indmax));
+        co2.(par)(indmin:indmax) = interp1(o2.DAYD, o2.(par), co2.DAYD(indmin:indmax));
         
     end % end for
 end
