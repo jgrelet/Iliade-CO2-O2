c0f04bf4ff4a128a9198bf571c5e4e8c987ab7b6
YannLeNorment <yann.lenorment@gmail.com>
2020-06-09 17:14:05 +0200

    Mise en place des calculs à réaliser pour corriger la concentration en o2

--- correctO2Data.m
+++ correctO2Data.m
@@ -1,6 +1,62 @@
-function correctO2Data
-
+function [co2] = correctO2Data(co2, salinity)
+    % co2 : the co2 structure
+    % salinity : 
+    S = co2.SSPS; % salinity
+    T = co2.SSJT; % temperature
+    P = co2.LICOR_P; % pressure
+    O2 = co2.OXYGEN_RAW; % raw oxygen
+    depth = 0;
     
-
-
+    CpC = 0.032; % Coeficient for pressure compensation
+    %
+    A0 = 2.00856;
+    A1 = 3.22400;
+    A2 = 3.99063;
+    A3 = 4.80299;
+    A4 = 9.78188e-1;
+    A5 = 1.71069;
+    % Sailinity compensation coeficient
+    B0 = -6.24097e-3;
+    B1 = -6.93498e-3;
+    B2 = -6.90358e-3;
+    B3 = -4.29155e-3;
+    
+    C0 = -3.11680e-7;
+    x = 298.15-T;
+    y = 273.15+T;
+    scaledTemperature = log(x/y);
+    
+    solubility = (P/1013.25) * 44.659 * ...
+        A0 + ...
+        A1 * +...
+        A2 * scaledTemperature^2+...
+        A3 * scaledTemperature^3+...
+        A4 * scaledTemperature^4+...
+        A5 * scaledTemperature^5+...
+        S(B0 + ...
+        B1 * scaledTemperature + ...
+        B2 * scaledTemperature^2 + ...
+        B3 * scaledTemperature^3 ) + ...
+        C0*S^2;
+        
+    salinityCompensation = exp(...
+        (S - salinity) * ...
+        (B0 + ...
+        B1 * scaledTemperature + ...
+        B2 * scaledTemperature^2 + ...
+        B3 * scaledTemperature^3) + ...
+        C0 * (S^2-salinity^2)...
+        );
+    
+    depthCompensation = O2 * (1 + (0.032*depth)/1000);
+    
+    pressureCompensation = ((abs(depth) /1000) * CpC) +1
+    
+    o2Concentration = O2*salinityCompensation * pressureCompensation;
+    
+    o2Saturation = (O2 * 2.2414) / solubility;
+    
+    co2.OXYGEN_ADJ = ones(size(co2.SSJT));
+    co2.OXYGEN_ADJ = o2Concentration;
+    
 end
