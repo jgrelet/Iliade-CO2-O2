86a63660549b3d27dc02e7e4becaf642e9ef3fb3
YannLeNorment <yann.lenorment@gmail.com>
2020-06-09 14:36:55 +0200

    Déplacement de la fonction propre d'interpolation vers un script afin de pouvoir tester son résultat

--- interpAll.m
+++ interpAll.m
@@ -1,4 +1,4 @@
-function interpTSG_CO2_O2(co2interpCo2_O2File, o2File, varargin)
+function interpAll(co2interpCo2_O2File, o2File, varargin)
     %
     % interpCo2_O2olation of O2 measure at co2 dates.
     % At the end, we will add the following columns to the .csv:
@@ -65,9 +65,8 @@ function interpTSG_CO2_O2(co2interpCo2_O2File, o2File, varargin)
                 'EQU_PUMP;VENT_FLOW;COND_T;COND_ATM;COND_EQU;'...
                 'DRIP_1;DRIP_2;DRY_BOX_T;DECK_BOX_T;'...
                 'SSPS;SSPS_QC;SSJT;SSJT_QC;SSJT_COR;EQU_T_COR;'...
-                'OXYGEN_RAW;SATURATION;O2_TEMPERATURE;'...
+                'OXYGEN_RAW;OXYGEN_ADJ;SATURATION;O2_TEMPERATURE;'...
                 ];
-                %'OXYGEN_RAW;OXYGEN_ADJ;SATURATION;O2_TEMPERATURE;'...
             
     StrucOrder = {'DATE';'GPS_TIME';'TYPE';'ERROR';'LATX';'LONX';...
                 'LATX_INT';'LONX_INT';...
@@ -101,13 +100,12 @@ function interpTSG_CO2_O2(co2interpCo2_O2File, o2File, varargin)
         msg_error = ['Open file error : ' FileOut];
         warndlg( msg_error, 'ASCII error dialog');
     else
-        gps_time = char(cellstr(co2.GPS_TIME));
-        type = char(cellstr(co2.TYPE));
-        
+        co2.GPS_TIME = char(cellstr(co2.GPS_TIME));
+        co2.TYPE = char(cellstr(co2.TYPE));
         
         c = struct2cell(co2);
         % Number of columns
-        n = size(c,1);
+        n = size(c, 1);
         % Number of line 
         m = size(c{1},1);
 
@@ -117,60 +115,13 @@ function interpTSG_CO2_O2(co2interpCo2_O2File, o2File, varargin)
         date = [D,M,Y,H,MN,S];
 
         fprintf( fidOut,'%s\n', HeaderOut);
-        disp(size(x));
         for i=1:m
-            fprintf( fidOut, formatOut, date(i,:), gps_time(i,:), type(i,:),x(i,:));
+            fprintf( fidOut, formatOut, date(i,:), co2.GPS_TIME(i,:), co2.TYPE(i,:),x(i,:));
         end
         
         fclose(fidOut);
     end
 end
 
-
-function [co2] = interpCo2_O2(co2, o2)
-
-    PARA = {'OXYGEN_RAW', 'SATURATION', 'TEMPERATURE'};
-    % Conversion of the date in serial date
-    format = 'dd/mm/yyyy HH:MM:SS';
-    co2.DAYD = datenum(co2.DATE_TIME, format);
-    for par = PARA
-        par = char(par);
-        
-        switch par
-            case 'OXYGEN_RAW'
-                defaultPARA = -1;
-            case 'SATURATION'
-                defaultPARA = -1;
-            case 'TEMPERATURE'
-                defaultPARA = -1;
-            otherwise
-                disp('Nom de variable inconnu');
-                return;
-        end
-        
-        co2.(par)    = defaultPARA * ones(size(co2.DAYD));
-        
-        % Definition of time borders for the interpCo2_O2olation of co2 and o2 data
-        % We are looking for o2.DAYD(1) < co2.DAYD < o2.DAYD(end)
-        indmin = 1;
-        if co2.DAYD(1) < o2.DAYD(1)
-            A = find( co2.DAYD > o2.DAYD(1) );
-            if ~isempty(A)
-                indmin = A(1);
-            end
-        end
-
-        indmax = size(co2.DAYD,1);
-        if co2.DAYD(end) > o2.DAYD(end)
-            A = find( co2.DAYD < o2.DAYD(end) );
-            if ~isempty(A)
-                indmax = A(end);
-            end
-        end
-        
-        co2.(par)(indmin:indmax) = interp1(o2.DAYD, o2.(par), co2.DAYD(indmin:indmax));
-        
-    end % end for
-end
     
 
---
+++ interpCo2_O2.m
@@ -0,0 +1,45 @@
+function [co2] = interpCo2_O2(co2, o2)
+
+    PARA = {'OXYGEN_RAW', 'SATURATION', 'TEMPERATURE'};
+    % Conversion of the date in serial date
+    format = 'dd/mm/yyyy HH:MM:SS';
+    co2.DAYD = datenum(co2.DATE_TIME, format);
+    for par = PARA
+        par = char(par);
+        
+        switch par
+            case 'OXYGEN_RAW'
+                defaultPARA = -1;
+            case 'SATURATION'
+                defaultPARA = -1;
+            case 'TEMPERATURE'
+                defaultPARA = -1;
+            otherwise
+                disp('Nom de variable inconnu');
+                return;
+        end
+        
+        co2.(par)    = defaultPARA * ones(size(co2.DAYD));
+        
+        % Definition of time borders for the interpCo2_O2olation of co2 and o2 data
+        % We are looking for o2.DAYD(1) < co2.DAYD < o2.DAYD(end)
+        indmin = 1;
+        if co2.DAYD(1) < o2.DAYD(1)
+            A = find( co2.DAYD > o2.DAYD(1) );
+            if ~isempty(A)
+                indmin = A(1);
+            end
+        end
+
+        indmax = size(co2.DAYD,1);
+        if co2.DAYD(end) > o2.DAYD(end)
+            A = find( co2.DAYD < o2.DAYD(end) );
+            if ~isempty(A)
+                indmax = A(end);
+            end
+        end
+        
+        co2.(par)(indmin:indmax) = interp1(o2.DAYD, o2.(par), co2.DAYD(indmin:indmax));
+        
+    end % end for
+end
