b072bec64396cd23e9e093b75162bb3a71447065
Jacques Grelet <jacques.grelet@ird.fr>
2020-06-11 16:28:12 +0200

    remove runtests

--- runtests.m
+++
@@ -1,9 +0,0 @@
-function result = runtests
-import matlab.unittest.TestSuite
-suiteFolder = TestSuite.fromFolder('tests');
-
-if nargout == 1
-  result = run(suiteFolder);
-else
-  run(suiteFolder);
-end
