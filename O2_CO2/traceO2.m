77f1fadad3d0be03381cc13361fc260e2a66959f
YannLeNorment <yann.lenorment@gmail.com>
2020-06-26 15:43:43 +0200

    Ajout du tracé de carte avec une colormap pour l'oxygen en µM

--- O2_CO2/traceO2.m
+++ O2_CO2/traceO2.m
@@ -50,32 +50,67 @@ function traceO2(fileIn)
         co2.(char(varNames(i))) = co2Data.(char(varNames(i)));
     end
     
-    disp("... Getting min and max lat/lon");
+    colors = colormap(jet);
+    % Get only the real data
+    data = real(co2.OXYGEN_ADJ_muM);
+    %disp(co2);
+    maxVal = max(data);
+    ind = data > 0;
+    data = data(ind);
+    step = maxVal / size(colors,1);
+    
+    disp("... Printing map");
     margin = 10;
     maxLat = max(co2.LATX);
     minLat = min(co2.LATX);
     
     maxLon = max(co2.LONX);
     minLon = min(co2.LONX);
-    
-    m_proj('mercator','longitudes', [minLon - margin, maxLon + margin],...
-        'latitudes',[minLat - margin, maxLat + margin]);
+    map = figure('Name','map','NumberTitle','off');
+    m_proj('Mercator','longitudes', [minLon - margin maxLon + margin],...
+        'latitudes',[minLat - margin maxLat + margin]);
     m_coast('patch',[.7 .7 .7],'edgecolor','none');
     m_grid;
     
-    disp("... Defining color for data");
+    % Colorbar
+    m_contfbar( [.3 .7],.98, data,[-300:1210],...
+            'axfrac',.02,'endpiece','no','levels','match','edgecolor','none'); 
+    % Trip of the boat
+    m_line(co2.LONX, co2.LATX, 'color', 'black');
     
-    colors = colormap;
-    % Get only the real data
-    data = real(co2.OXYGEN_ADJ_muM);
-    %disp(co2);
-    maxVal = max(data);
-    ind = find(data > 0);
-    data = data(ind);
-    step = maxVal / size(colors,1);
-    idx = round((data ./ step), 0);
-    rdx = idx ~= 0;
-    cols = [ colors(idx(rdx), 1), colors(idx(rdx), 2), colors(idx(rdx), 3) ];
-    %m_line(co2.LONX, co2.LATX, 'color',...
-    %   cols );
+    for i = 1:size(colors)
+        ids = find(round((data ./ step), 0) == i);
+        m_line(co2.LONX(ids), co2.LATX(ids),'LineStyle', 'none', 'marker', 'o', 'markersize', 1 ,'color', colors(i,:));
+    end
+    % we close the fig one which is opened by m_proj
+    close Figure 1
+    
+    % OXYGEN µM
+    clear ind
+    clear dates
+    ind = co2.OXYGEN_ADJ_muM > 0;
+    dates = co2.DATE_TIME(ind);
+    dates = datetime(dates, 'Format', 'dd/MM/yyyy HH:mm:SS');
+
+    muM = figure('Name','Oxygen Compensated µM','NumberTitle','off');
+
+    plot(dates, co2.OXYGEN_ADJ_muM(ind), 'color','black');
+    title('Oxygen Compensated µM')
+    xlabel('Date')
+    ylabel('µM');
+    
+    % OXYGEN ml/l
+    clear ind
+    clear dates
+    ind = co2.OXYGEN_ADJ_MLL > 0;
+    dates = co2.DATE_TIME(ind);
+    dates = datetime(dates, 'Format', 'dd/MM/yyyy HH:mm:SS');
+    
+    mll = figure('Name','Oxygen Compensated ml/l','NumberTitle','off');
+    plot(dates, co2.OXYGEN_ADJ_MLL(ind), 'color','black');
+    title('Oxygen Compensated ml/l')
+    xlabel('Date')
+    ylabel('ml/l');
+    
+    figure(map);
 end
