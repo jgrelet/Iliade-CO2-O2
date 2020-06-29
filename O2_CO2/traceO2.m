51841be62dbca6c91caaf2f5001fb0c28cacaf20
YannLeNorment <yann.lenorment@gmail.com>
2020-06-29 14:59:59 +0200

    Commentaire sur la facon dont j'ai procédé

--- O2_CO2/traceO2.m
+++ O2_CO2/traceO2.m
@@ -49,42 +49,56 @@ function traceO2(fileIn)
     for i = 1:size(varNames,2)
         co2.(char(varNames(i))) = co2Data.(char(varNames(i)));
     end
-    
+    %% Map handle
     colors = colormap(jet);
     % Get only the real data
     data = real(co2.OXYGEN_ADJ_muM);
-    %disp(co2);
+    
     maxVal = max(data);
+    % We select the data > 0. The rest is the default data
     ind = data > 0;
     data = data(ind);
     step = maxVal / size(colors,1);
-    
+    maxVal = max(data);
     disp("... Printing map");
+    
+    % Allow a projection a bit bigger than the area in the file
     margin = 10;
+    
+    % Get the min and max latitudes
     maxLat = max(co2.LATX);
     minLat = min(co2.LATX);
-    
+    % Get the min and max longitudes
     maxLon = max(co2.LONX);
     minLon = min(co2.LONX);
-    map = figure('Name','map','NumberTitle','off');
+    
+    % Creating the figure for the map
+    map = figure('Name','map','NumberTitle','off',...
+        'Position',[1000 300 500 600]);
+    
+    % Use of the M_MAP library
     m_proj('Mercator','longitudes', [minLon - margin maxLon + margin],...
         'latitudes',[minLat - margin maxLat + margin]);
     m_coast('patch',[.7 .7 .7],'edgecolor','none');
     m_grid;
     
     % Colorbar
-    m_contfbar( [.3 .7],.98, data,[-300:1210],...
-            'axfrac',.02,'endpiece','no','levels','match','edgecolor','none'); 
+    % [posX posY] height, data, [gap]
+    m_contfbar( [.3 .7],.98, data,[0:maxVal],...
+            'axfrac',.02,'endpiece','no','edgecolor','none'); 
     % Trip of the boat
     m_line(co2.LONX, co2.LATX, 'color', 'black');
     
     for i = 1:size(colors)
+        % Select of the color depending on the value of O2 µM
         ids = find(round((data ./ step), 0) == i);
-        m_line(co2.LONX(ids), co2.LATX(ids),'LineStyle', 'none', 'marker', 'o', 'markersize', 1 ,'color', colors(i,:));
+        m_line(co2.LONX(ids), co2.LATX(ids),'LineStyle', 'none',...
+            'marker', 'o', 'markersize', 1 ,'color', colors(i,:));
     end
+    
     % we close the fig one which is opened by m_proj
     close Figure 1
-    
+    %% Display of curves
     % OXYGEN µM
     clear ind
     clear dates
@@ -92,7 +106,8 @@ function traceO2(fileIn)
     dates = co2.DATE_TIME(ind);
     dates = datetime(dates, 'Format', 'dd/MM/yyyy HH:mm:SS');
 
-    muM = figure('Name','Oxygen Compensated µM','NumberTitle','off');
+    muM = figure('Name','Oxygen Compensated µM','NumberTitle','off',...
+        'Position',[200 150 500 350]);
 
     plot(dates, co2.OXYGEN_ADJ_muM(ind), 'color','black');
     title('Oxygen Compensated µM')
@@ -106,7 +121,8 @@ function traceO2(fileIn)
     dates = co2.DATE_TIME(ind);
     dates = datetime(dates, 'Format', 'dd/MM/yyyy HH:mm:SS');
     
-    mll = figure('Name','Oxygen Compensated ml/l','NumberTitle','off');
+    mll = figure('Name','Oxygen Compensated ml/l','NumberTitle','off',...
+        'Position',[700 150 500 350]);
     plot(dates, co2.OXYGEN_ADJ_MLL(ind), 'color','black');
     title('Oxygen Compensated ml/l')
     xlabel('Date')
