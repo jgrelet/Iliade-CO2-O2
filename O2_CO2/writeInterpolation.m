79cfe0e3f2bf9e6f4b51e7ad46b7f3e34a59a794
YannLeNorment <yann.lenorment@gmail.com>
2020-06-29 14:15:14 +0200

    Tri des données par rapport à la date

--- O2_CO2/writeInterpolation.m
+++ O2_CO2/writeInterpolation.m
@@ -9,9 +9,23 @@ function interpFile = writeInterpolation(co2)
         msg_error = ['Open file error : ' FileOut];
         warndlg( msg_error, 'ASCII error dialog');
     else
-        disp(strcat("... Writing results in : ", interpFile));
+        disp(strcat("... Writing results in : ", interpFile, " Please wait ..."));
         % We remove the DAYD field
         co2 = rmfield(co2, 'DAYD');
+        % We create an additonnal field to sort the data by the date
+        % (some data were not sorted correctly)
+        co2.TEMP_DATETIME = datetime(co2.DATE_TIME,'InputFormat','dd/MM/yyyy HH:mm:ss');
+        
+        % In order to sort, we need a table
+        T = struct2table(co2);
+        sortedT = sortrows(T, 'TEMP_DATETIME');
+        
+        % We go back to a structure
+        co2 = table2struct(sortedT,'ToScalar',true);
+        % we remove the temp field
+        co2 = rmfield(co2, 'TEMP_DATETIME');
+        
+        % Wrting data to the file
         struct2csv(co2,interpFile);
         fclose(fidOut);
     end
